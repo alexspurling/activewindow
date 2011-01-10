3186b96b8bf181ac610aeb56dd8fa6093746393f
Alex Spurling <alex.spurling@playfish.com>
2011-01-10 22:54:31 +0000

    Reformatting activewindow.sh

--- activewindow.sh
+++ activewindow.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 function getCurWindowName {
-	read D1 D2 D3 D4 windowid <<< "`xprop -root _NET_ACTIVE_WINDOW`"
-	read D1 D2 windowname <<< "`xprop -id $windowid WM_NAME`"
+    read D1 D2 D3 D4 windowid <<< "`xprop -root _NET_ACTIVE_WINDOW`"
+    read D1 D2 windowname <<< "`xprop -id $windowid WM_NAME`"
 }
 
-logfile="activewindows"
+logfile="activewindow.log"
 
 #Create the active window log file if it doesn't already exist
 if [ ! -e "$logfile" ]
 then
-	touch "$logfile"
+    touch "$logfile"
 fi
 
 lastline=`tail -1 "$logfile"`
@@ -18,17 +18,17 @@ lastwindowname=`echo $lastline | awk '{print $2}'`
 
 while [ "true" ]
 do
-	getCurWindowName
+    getCurWindowName
 
-	if [ "$windowname" != "$lastwindowname" ]
-	then
-		curtime=$(($(date +%s%N)/1000000))
-		echo "$curtime $windowname" >> $logfile
-		echo "$curtime $windowname"
-		lastwindowname=$windowname
-	fi
+    if [ "$windowname" != "$lastwindowname" ]
+    then
+        curtime=$(($(date +%s%N)/1000000))
+        echo "$curtime $windowname" >> $logfile
+        echo "$curtime $windowname"
+        lastwindowname=$windowname
+    fi
 
-	sleep 1
+    sleep 1
 done
 
 
