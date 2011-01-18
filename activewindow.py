41ec81e0b975819253ad33d3a055448b1b8f9f05
Alex Spurling <alex.spurling@playfish.com>
2011-01-18 22:10:03 +0000

    Adding process name to output of active window monitor

--- activewindow.py
+++ activewindow.py
@@ -6,30 +6,47 @@ import datetime
 class ActiveWindow:
 
     #Taken from: http://stackoverflow.com/questions/3983946/get-active-window-title-in-x
-    def get_active_window_title(self):
+    def get_active_window(self):
         root = Popen(['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout=PIPE)
 
         for line in root.stdout:
             m = re.search('^_NET_ACTIVE_WINDOW.* ([\w]+)$', line)
             if m != None:
                 id_ = m.group(1)
-                id_w = Popen(['xprop', '-id', id_, 'WM_NAME'], stdout=PIPE)
+                id_w = Popen(['xprop', '-id', id_, '_NET_WM_PID', 'WM_NAME'], stdout=PIPE)
                 break
 
+        pid = 0
+        name = "Unknown"
+
         if id_w != None:
-            id_w.wait()
             for line in id_w.stdout:
+                match = re.match("_NET_WM_PID\([A-Z]+\) = (?P<pid>.+)$", line)
+                if match != None:
+                    pid = match.group("pid")
+
                 match = re.match("WM_NAME\([A-Z]+\) = (?P<name>.+)$", line)
                 if match != None:
-                    return match.group("name")
+                    name = match.group("name")
 
-        return "Unknown"
+        return (pid, name)
+
+    def get_process_name_from_id(self, processid):
+        if processid == 0:
+            return ""
+
+        with open("/proc/%s/stat" % processid, "r") as f:
+            processstat = f.readline()
+
+        match = re.match("^%s ([^\s]*) .*" % processid, processstat)
+        processname = match.group(1)
+        return processname
         
     def get_current_time(self):
         return "%d" % (time.time() * 1000)
         
-    def log_window(self, f, currentwindow):
-        logstring = "%s %s\n" % (self.get_current_time(), currentwindow)
+    def log_window(self, f, currentprocess, currentwindow):
+        logstring = "%s %s %s\n" % (self.get_current_time(), currentprocess, currentwindow)
         f.write(logstring)
         f.flush()
 
@@ -41,14 +58,15 @@ class ActiveWindow:
             
             try:
                 while True:
-                    currentwindow = self.get_active_window_title()
+                    curpid, currentwindow = self.get_active_window()
                     if currentwindow != lastwindow:
-                        self.log_window(f, currentwindow)
+                        currentprocess = self.get_process_name_from_id(curpid)
+                        self.log_window(f, currentprocess, currentwindow)
                         lastwindow = currentwindow
                     
                     time.sleep(1)
             except KeyboardInterrupt:
-                self.log_window(f, "ActiveWindow terminated")
+                self.log_window(f, "(activewindow)", "ActiveWindow terminated")
                 print ""
 
 aw = ActiveWindow()
