1484009a0c362cd43ad742b6a6e3c25ffd1f3b3c
Alex Spurling <alex.spurling@playfish.com>
2011-01-24 23:21:05 +0000

    Grouping window times by command

--- activewindowsummary.py
+++ activewindowsummary.py
@@ -1,19 +1,19 @@
 
 import re
 from operator import itemgetter
+from itertools import groupby
 
 logfile = "activewindow.log"
 f = open(logfile, 'r')
 
-windowtimes = {}
+commandtimes = {}
 
 lasttime = 0
 
 def get_all_string_tokens(str):
     alltokens = re.split("[^\w]+", str)
     nonemptytokens = [token for token in alltokens if len(token) > 0]
-    print "Split: %s" % nonemptytokens
-    return get_all_list_tokens(nonemptytokens)
+    return set(get_all_list_tokens(nonemptytokens))
 
 def get_all_list_tokens(list):
     if len(list) <= 1:
@@ -27,15 +27,21 @@ def get_all_list_tokens(list):
         tokenlist.extend(get_all_list_tokens(list[1:]))
         return tokenlist
 
-def record_window_time(titlestr, time):
+def record_window_time(command, titlestr, time):
+    #Get the window times for this command
+    windowtimes = commandtimes.get(command, {})
+    commandtimes[command] = windowtimes
+
+    windowtimes[titlestr] = time
+
+    #Break the title up into word groups
     alltitletokens = get_all_string_tokens(titlestr)
     
+    #For each word group string, record the time for this command
     for titletoken in alltitletokens:
-        totaltime = time
-        if titletoken in windowtimes:
-            totaltime += windowtimes[titletoken]
-
-        windowtimes[titletoken] = totaltime
+        windowtimes[titletoken] = windowtimes.get(titletoken, 0) + time
+        if command == "/usr/bin/gedit":
+            print "titletoken: %s, time: %s, totaltime: %s" % (titletoken, time, windowtimes[titletoken])
 
 for line in f:
 
@@ -48,12 +54,18 @@ for line in f:
 
         if lasttime != 0:
             time = curtime - lasttime
-            print "Title: %s time: %s" % (title, time)
-            record_window_time(title, time)
+            print "Title: %s command: %s, time: %s" % (title, command, time)
+            record_window_time(command, title, time)
 
         lasttime = curtime
 
-
-sortedtimes = sorted(windowtimes.iteritems(), key=itemgetter(1), reverse=True)
+for command in commandtimes:
+    windowtimes = commandtimes[command]
+    sortedwindowtimes = sorted(windowtimes.iteritems(), key=itemgetter(1), reverse=True)
+    
+    for timespent, windowtitles in groupby(sortedwindowtimes, lambda key: key[1]):
+        for title in windowtitles:
+            print "Title %s, time: %s" % (title, timespent)
+        print " "
 
 print sortedtimes
